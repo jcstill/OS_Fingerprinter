37df70f440c4932153cc8e236757884b1a84d7d6
jcstill <31301328+jcstill@users.noreply.github.com>
2018-03-22 16:42:37 -0700

    Update fingeredusb.sh

--- fingeredusb.sh
+++ fingeredusb.sh
@@ -5,6 +5,12 @@
 #	but this is how i did it
 #
 #	the comment blocks are mainly for me to know what i want to add
+#
+#	BUGS: 	this script is started from /etc/rc.local through a launcher
+#		script (that waits for 30 sec for the boot process to finish)
+#		however, the results of the scan (in scan_#.txt while plugged 
+#		into a computer is empty (maybe no data on port at time?)
+#		
 #-------------------------------------------------------------------------------------
 #!/bin/bash
 version=0.1
